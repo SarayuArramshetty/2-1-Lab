0ef60c97df35fbd0851a21ec5135fa4471ef97e0
SarayuArramshetty <97151471+SarayuArramshetty@users.noreply.github.com>
2022-02-08 12:29:00 +0530

    Create binaryOperator.cpp

---
+++ C++ Lab/Experiment 15b/binaryOperator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+class A{
+    int x;
+    public:
+    A(){}
+    A(int i){
+        x=i;
+    }
+    void operator+(A);
+};
+void A :: operator+(A a){
+    int m=x+a.x;
+    cout<<"The result of the addition of two objects is:"<<m;
+    
+}
+
+int main()
+{
+    A a1(5);
+    A a2(4);
+    a1+a2;
+
+    return 0;
+}
